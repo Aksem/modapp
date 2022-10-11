dde19f4633a278cd3452984ae6c83b3a47b95798
Vladyslav Hnatiuk <aders1234@gmail.com>
2022-10-11 06:38:39 +0200

    v0.1.15: update deps, improve enum handling

--- modapp/communication_utils.py
+++ modapp/communication_utils.py
@@ -1,6 +1,6 @@
 import orjson
 from datetime import timezone
-from typing import Dict, Any, Optional
+from typing import Dict, Any, List, Optional
 
 from asyncio import iscoroutine
 from loguru import logger
@@ -28,6 +28,7 @@ def deserialize_request(route: Route, data: bytes):
     #     if (field.name not in request_dict
     #         and request_schema['properties'][field.name].get('type', None) == 'string')
     # })
+
     # protobuff ListFields method doesn't return boolean fields if they have value False)
     request_dict.update(
         {
@@ -41,6 +42,31 @@ def deserialize_request(route: Route, data: bytes):
             )
         }
     )
+
+    # protobuff ListFields method doesn't return enum fields if they have value 0)
+    fields_to_update: List[str] = []
+    for field in proto_request.DESCRIPTOR.fields:
+        if (
+            field.name not in request_dict
+            and field.name in request_schema["properties"]
+            and request_schema["properties"][field.name].get("$ref", None) is not None
+        ):
+            definition_name = (
+                request_schema["properties"][field.name].get("$ref", "").split("/")[-1]
+            )
+            try:
+                # can ref be imported? TODO: check
+                definition = request_schema["definitions"][definition_name]
+            except KeyError:
+                logger.warning(
+                    f"Field '{field.name}' has reference to definition, but definition was not found"
+                )
+                continue
+
+            if definition["type"] == "integer" and "enum" in definition:
+                fields_to_update.append(field.name)
+    request_dict.update({field: 0 for field in fields_to_update})
+
     # request is not neccessary valid utf-8 string, handle errors
     logger.trace(str(request_dict).encode("utf-8", errors="replace"))
     try:
@@ -68,23 +94,26 @@ async def run_request_handler(route, handler_arguments: Dict[str, Any]) -> BaseM
 def serialize_reply(route: Route, reply: Optional[BaseModel]) -> bytes:
     if reply is None:
         logger.error(f"Route handler '{route.path}' doesn't return value")
-        raise ServerError('Internal error')
+        raise ServerError("Internal error")
 
     json_reply = orjson.loads(reply.json(by_alias=True))
-    
+
     def fix_json(model, json):
         model_schema = type(model).schema()
         for field in model.__dict__:
             # convert datetime to google.protobuf.Timestamp instance
             # in pydantic model schema datetime has type 'string' and format 'date-time'
-            if model_schema["properties"][to_camel(field)].get("format", None) == "date-time":
+            if (
+                model_schema["properties"][to_camel(field)].get("format", None)
+                == "date-time"
+            ):
                 json[to_camel(field)] = Timestamp(
                     seconds=int(
                         model.__dict__[field].replace(tzinfo=timezone.utc).timestamp()
                     )
                     # TODO: nanos?
                 )
-            elif '$ref' in model_schema["properties"][to_camel(field)]:
+            elif "$ref" in model_schema["properties"][to_camel(field)]:
                 # model reference, fix recursively
                 fix_json(model.__dict__[field], json[to_camel(field)])
 
