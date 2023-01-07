bcd1e0a1a69f78e166fdf026cc70ae129c03cae3
Vladyslav Hnatiuk <aders1234@gmail.com>
2023-01-07 09:50:19 +0100

    Export base converter

--- modapp/__init__.py
+++ modapp/__init__.py
@@ -1,6 +1,7 @@
+from .base_converter import BaseConverter
 from .base_transport import BaseTransportConfig, BaseTransport
 from .param_functions import Meta, Depends
 from .routing import APIRouter
 from .server import Modapp
 
-__all__ = ["Modapp", "BaseTransport", "BaseTransportConfig", "APIRouter", "Meta", "Depends"]
+__all__ = ["Modapp", "BaseConverter", "BaseTransport", "BaseTransportConfig", "APIRouter", "Meta", "Depends"]
