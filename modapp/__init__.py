ed8e566250221adf3e9aee384ef8be1325cfe741
Vladyslav Hnatiuk <aders1234@gmail.com>
2023-01-07 09:48:59 +0100

    Add Depends() (in progress, can be defined, but isn't executed)

--- modapp/__init__.py
+++ modapp/__init__.py
@@ -1,6 +1,6 @@
 from .base_transport import BaseTransportConfig, BaseTransport
-from .param_functions import Meta
+from .param_functions import Meta, Depends
 from .routing import APIRouter
 from .server import Modapp
 
-__all__ = ["Modapp", "BaseTransport", "BaseTransportConfig", "APIRouter", "Meta"]
+__all__ = ["Modapp", "BaseTransport", "BaseTransportConfig", "APIRouter", "Meta", "Depends"]
