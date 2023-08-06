8b8a2740a3059cc33c4e9b40e4f3717e58241c72
Vladyslav Hnatiuk <aders1234@gmail.com>
2023-08-06 10:13:25 +0200

    Add missing tests of protobuf.model_to_raw

--- tests/converters/protobuf/test__protobuf__model_to_raw.py
+++ tests/converters/protobuf/test__protobuf__model_to_raw.py
@@ -12,15 +12,57 @@ class TestProtobufConverterRawToModel(ProtobufConverterBaseTestSuite):
 
         assert raw_data == context.proto_instance.SerializeToString()
 
+    def test_defaults(self, tmp_path: Path) -> None:
+        context = self.arrange_test_defaults(tmp_path=tmp_path)
+
+        raw_data = context.converter.model_to_raw(context.model_instance_ref)
+
+        assert raw_data == context.proto_instance.SerializeToString()
+
     def test_nested_messages(self, tmp_path: Path) -> None:
         context = self.arrange_test_nested_messages(tmp_path)
 
-        raw_data = context.converter.model_to_raw(model=context.model_instance)
+        raw_data = context.converter.model_to_raw(model=context.model_instance_ref)
 
         assert raw_data == context.proto_instance.SerializeToString()
 
     def test_nested_message_repeated(self, tmp_path: Path) -> None:
         context = self.arrange_nested_message_repeated_test(tmp_path)
+
+        raw_data = context.converter.model_to_raw(model=context.model_instance_ref)
+
+        assert raw_data == context.proto_instance.SerializeToString()
+
+    def test_scalar_repeated(self, tmp_path: Path) -> None:
+        context = self.arrange_scalar_repeated_test(tmp_path)
+
+        raw_data = context.converter.model_to_raw(model=context.model_instance_ref)
+
+        assert raw_data == context.proto_instance.SerializeToString()
+
+    def test_message_repeated(self, tmp_path: Path) -> None:
+        context = self.arrange_scalar_repeated_test(tmp_path)
+
+        raw_data = context.converter.model_to_raw(model=context.model_instance_ref)
+
+        assert raw_data == context.proto_instance.SerializeToString()
+
+    def test_one_of_scalars(self, tmp_path: Path) -> None:
+        context = self.arrange_one_of_scalars_test(tmp_path)
+
+        raw_data = context.converter.model_to_raw(model=context.model_instance_ref)
+
+        assert raw_data == context.proto_instance.SerializeToString()
+
+    def test_one_of_defaults(self, tmp_path: Path) -> None:
+        context = self.arrange_one_of_scalars_test(tmp_path)
+
+        raw_data = context.converter.model_to_raw(model=context.model_instance_ref)
+
+        assert raw_data == context.proto_instance.SerializeToString()
+
+    def test_one_of_nested_messages(self, tmp_path: Path) -> None:
+        context = self.arrange_one_of_scalars_test(tmp_path)
 
         raw_data = context.converter.model_to_raw(model=context.model_instance_ref)
 
