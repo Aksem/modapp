7b4d3fce2afac560653ace2b984a846c8854e434
Vladyslav Hnatiuk <aders1234@gmail.com>
2024-07-07 11:06:15 +0200

    Preparation for background tasks: background module

---
+++ modapp/background/utils.py
@@ -0,0 +1,11 @@
+import asyncio
+import functools
+import typing
+
+def is_async_callable(obj: typing.Any) -> typing.Any:
+    while isinstance(obj, functools.partial):
+        obj = obj.func
+
+    return asyncio.iscoroutinefunction(obj) or (
+        callable(obj) and asyncio.iscoroutinefunction(obj.__call__)
+    )
