7342414d93b8a137b118e7459335d78d0d3064a1
Vladyslav Hnatiuk <aders1234@gmail.com>
2024-10-21 16:27:42 +0200

    Make error_details optional in grpc transport

--- modapp/transports/grpc_config.py
+++ modapp/transports/grpc_config.py
@@ -6,10 +6,13 @@ from modapp.base_transport import BaseTransportConfig
 class GrpcTransportConfig(BaseTransportConfig):
     address: NotRequired[str]
     port: NotRequired[int]
+    # not all clients supports error details passed in response metadata. Enables this feature
+    error_details: NotRequired[bool]
 
 
 DEFAULT_CONFIG: GrpcTransportConfig = {
     "address": "127.0.0.1",
     "port": 50051,
     "max_message_size_kb": 4096,
+    "error_details": False
 }
